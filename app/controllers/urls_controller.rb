7fe4290643dc9db2034cd0f3114906e27192861b
teeyingyap <yapteeying@gmail.com>
2017-04-25 12:55:00 +0800

    1. create partial view file 2. set up new action 3. set up create action

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -7,4 +7,24 @@ class UrlsController < ApplicationController
     @url = Url.find(params[:id])
   end
 
+  def new
+    @url = Url.new
+  end
+  
+  def create
+    @url = Url.new(url_params)
+    @url.shorten
+    if @url.save
+      redirect_to @url  
+    else
+      render 'new'
+    end   
+  end
+
+  private 
+
+  def url_params
+    params.require(:url).permit(:long_url)
+  end
+
 end
